12ef1b88745a7b6413465a3c84754a1641fa2781
twoonefour <lys214412@gmail.com>
2025-12-25 19:35:50 +0800

    1

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -570,6 +570,21 @@ onMounted(() => {
         </div>
 
         <div style="display: flex; align-items: center; gap: 20px; min-width: 120px; justify-content: flex-end;">
+          <n-button
+              quaternary
+              circle
+              tag="a"
+              href="https://github.com/twoonefour/campustrader.git"
+              target="_blank"
+          >
+            <template #icon>
+              <n-icon size="24">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <path d="M12 2C6.477 2 2 6.477 2 12c0 4.419 2.865 8.166 6.839 9.489c.5.092.682-.217.682-.482c0-.237-.008-.866-.013-1.7c-2.782.604-3.369-1.34-3.369-1.34c-.454-1.156-1.11-1.463-1.11-1.463c-.908-.62.069-.008.069-.008c1.003.07 1.531 1.03 1.531 1.03c.892 1.529 2.341 1.087 2.91.832c.092-.647.35-1.088.636-1.338c-2.22-.253-4.555-1.11-4.555-4.943c0-1.091.39-1.984 1.029-2.683c-.103-.253-.446-1.27.098-2.647c0 0 .84-.269 2.75 1.025A9.578 9.578 0 0 1 12 6.836c.85.004 1.705.114 2.504.336c1.909-1.294 2.747-1.025 2.747-1.025c.546 1.377.203 2.394.1 2.647c.64.699 1.028 1.592 1.028 2.683c0 3.842-2.339 4.687-4.566 4.935c.359.309.678.92.678 1.855c0 1.338-.012 2.419-.012 2.747c0 .268.18.579.688.481C19.137 20.161 22 16.416 22 12c0-5.523-4.477-10-10-10z" fill="currentColor"/>
+                </svg>
+              </n-icon>
+            </template>
+          </n-button>
           <div v-if="token">
             <n-dropdown :options="userDropdownOptions" @select="handleUserDropdown">
               <div style="display: flex; align-items: center; cursor: pointer; gap: 10px;">
